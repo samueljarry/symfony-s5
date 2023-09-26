40302767c9dcb781e143562eac076735ca77b9cb
samueljarry <samueljarry.sj@gmail.com>
2023-09-26 09:32:55 +0200

    Feat: Add Nationality Entity & Fixtures

---
+++ src/DataFixtures/NationalityFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Nationality;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class NationalityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $nationalities = [
+            "Française",
+            "Espagnole",
+            "Italienne",
+            "Allemande",
+            "Britannique",
+            "Irlandaise",
+            "Portugaise",
+            "Belge",
+            "Suédoise",
+            "Norvégienne",
+            "Danoise",
+            "Grecque",
+            "Russe",
+            "Polonaise",
+            "Ivoirienne",
+            "Marocaine",
+            "Algérienne",
+            "Tunisienne",
+            "Sénégalaise",
+            "Américaine",
+            "Canadienne",
+            "Brésilienne",
+            "Argentine",
+            "Chilienne",
+            "Mexicaine",
+            "Japonaise",
+            "Chinoise",
+            "Indienne",
+            "Australienne",
+            "Néo-zélandaise"
+        ];
+
+        foreach (range(1, 27) as $i) {
+            $nationality = new Nationality();
+            $nationality->setName($nationalities[$i]);
+            $manager->persist($nationality);
+            $this->addReference('nationality_' . $i, $nationality); // "expose" l'objet à l'extérieur de la classe pour les liaisons avec Movie
+        }
+
+        $manager->flush();
+    }
+}
